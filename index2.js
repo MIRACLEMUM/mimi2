86f8ca534e29d9effa7702448611e68d5de77657
Miracle sunady <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2025-04-04 08:24:22 +0100

    hhh

--- index2.js
+++ index2.js
@@ -1,24 +1,33 @@
-const contentEl = document.querySelector(".content")
+const contentEl = document.querySelector(".content");
 
-const careers = ['web developer','freelancer','youtuber'];
+const careers = ['Web Developer', 'Freelancer', 'YouTuber','content creator'];
 
+let careerIndex = 0;
 let characterIndex = 0;
+let isDeleting = false;
+
+function updateText() {
+    const currentCareer = careers[careerIndex];
+    let displayedText;
+
+    if (isDeleting) {
+        displayedText = currentCareer.slice(0, characterIndex--);
+    } else {
+        displayedText = currentCareer.slice(0, characterIndex++);
+    }
+
+    contentEl.innerHTML = `<h1>I am a ${displayedText}</h1>`;
+
+    if (!isDeleting && characterIndex === currentCareer.length + 1) {
+        isDeleting = true;
+        setTimeout(updateText, 1000); // Pause before deleting
+    } else if (isDeleting && characterIndex === 0) {
+        isDeleting = false;
+        careerIndex = (careerIndex + 1) % careers.length; // Move to the next career
+        setTimeout(updateText, 500);
+    } else {
+        setTimeout(updateText, isDeleting ? 100 : 200); // Faster deleting, slower typing
+    }
+}
 
 updateText();
-
-function updateText(){
-characterIndex++
-   
-contentEl.innerHTML = `<h1>I am a ${careers [careerindex].slice(0,characterIndex)}</h1>
-`;
-if(characterIndex === careers[careerindex].length){
-    careerindex++
-    characterIndex = 0;
-}
-
-if (careerIndex === careers.length)
-{
-  careerIndex = 0;
-}
-setTimeout(updateText,400)
-}
